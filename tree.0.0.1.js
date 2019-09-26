25261f2957dcb44dee136b2a34f79afde1305f7c
sevenJia <36792933+sevenJia@users.noreply.github.com>
2019-09-26 14:53:37 +0800

    0.0.1

--- tree.0.0.1.js
+++ tree.0.0.1.js
@@ -1,10 +1,8 @@
 function convertToTreeData(rows){
-  var map = {};
-  var treeResult = [];
-  rows.forEach((item) => map[item.id] = item)
-  rows.forEach((item) => {
-      var parentNode = map[item.upper_id];
-      parentNode != undefined ? (parentNode.children || ( parentNode.children = [] )).push(item) : treeResult.push(item);
-  })
-  return treeResult;
-}
+		let map = {},treeResult = [];
+		rows.forEach(item => (map[item.id] = item))
+		rows.forEach(item => {
+		  map[item.pid] != undefined ? (map[item.pid].children || ( map[item.pid].children = [] )).push(item) : treeResult.push(item);
+		})
+		return treeResult;
+	}
